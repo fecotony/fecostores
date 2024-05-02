8029d10a211a7b2298efb3e1a7807bd5ed39dd06
fecotony <47065253+fecotony@users.noreply.github.com>
2024-05-01 21:48:05 -0700

    save for now

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
